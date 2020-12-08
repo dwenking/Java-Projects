4022ed6e23958a3d915f6e132944e390441106ef
iimchaos <53037214+iimchaos@users.noreply.github.com>
2020-12-08 16:00:41 +0800

    update 12.8

---
+++ GrabData/src/main/java/JDKHttpClientGetTest.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
+
+public class JDKHttpClientGetTest {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		doGet();
+	}
+	
+	public static void doGet() {
+		try{
+			HttpClient client = HttpClient.newHttpClient();
+			HttpRequest request = HttpRequest.newBuilder(URI.create("https://faculty.ecnu.edu.cn/")).build();
+			HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			System.out.println(response.body());
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
